12218dc4fa83361c37562d893a0d6692432295a8
Tony Vo <tonyducvo000@gmail.com>
2017-07-05 21:41:00 -0700

    Modularized .html and .jpg files.  Added description.  Changed list file name to LIST.

--- image_grab.sh
+++ image_grab.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-#Because CL uses JS to display images, simple wget command to get the files will not work
+#Because Craigslist uses Javascript to display images, a simple wget command to get the pictures will not work.
+
 
 #Get all posted items for sale
-wget -w 3 --level=1 -nd -rkH -e robots=off -A .html https://orangecounty.craigslist.org/search/rea
+mkdir $(uuid) && cd $_ && wget -w 3 --level=1 -nd -rkH -e robots=off -A .html https://orangecounty.craigslist.org/search/rea
 
-#Parses each file and extracts the url link to the picture (if there is any) and dumps it into pictures.txt
-grep -h -r -e '(?<=content\=\")(https.*\.jpg)' -oP * >> pictures.txt
+#Parses each file and extracts the url link to the picture (if there are any) and dumps it into LIST
+grep -h -r -e '(?<=\"url\"\:\")(https.*?\.jpg)' -oP * >> ../LIST
 
-#Downloads the image from the list pictures.txt
-wget -i pictures.txt
+#Downloads the image from LIST
+cd .. && mkdir "$(uuid)_pictures" && cd $_ && wget -i ../LIST
